2047f314e501c9a7890cf9601e749f2f71211354
James Quintero <jquinte2@mail.sfsu.edu>
2017-03-15 22:26:36 -0700

    Converted tabs to spaces for nicer formatting on github

--- C++/ShiftSort.h
+++ C++/ShiftSort.h
@@ -1,11 +1,11 @@
 #include <iostream>
 
 class ShiftSort{
-	private:
-		void split(int*, int*, int, int);
-		void merge(int*, int, int, int);
-	public:
-		ShiftSort();
-		void sort(int*, int);
-	
+    private:
+        void split(int*, int*, int, int);
+        void merge(int*, int, int, int);
+    public:
+        ShiftSort();
+        void sort(int*, int);
+  
 };
